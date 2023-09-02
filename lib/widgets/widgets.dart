cd3e21d2816ec7b4738c60f37943441b44ace7ef
SebasVillaLo <villalopezsebastian@gmail.com>
2023-09-02 02:04:34 -0500

    Barrel file to widgets

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'shared/card_character_view.dart';
+export 'shared/card_view.dart';
+export 'shared/list_details.dart';
+export 'shared/loading_circular.dart';
+export 'shared/navigationbar_custom.dart';
+export 'shared/scaffold_with_appbar.dart';
