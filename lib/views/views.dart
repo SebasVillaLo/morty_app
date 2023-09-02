0e504320ee970c1a5cafa685e5c95848c60b9703
SebasVillaLo <villalopezsebastian@gmail.com>
2023-09-02 02:01:19 -0500

    Barrel file to views

---
+++ lib/views/views.dart
@@ -0,0 +1,6 @@
+export 'characters/characters_details.dart';
+export 'characters/characters_view.dart';
+export 'episode/episode_details.dart';
+export 'episode/episode_view.dart';
+export 'location/location_details.dart';
+export 'location/location_view.dart';
